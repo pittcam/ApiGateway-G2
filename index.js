f36533c2fe3de8bed3118692d0b70feef4aa996a
Juanba2002 <bajuan00@gmail.com>
2025-04-10 22:07:45 -0500

    Cambio

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const schema = await stitchSchemas({
         loaders: [new UrlLoader()],
       }),
     }
-    // Aquí puedes agregar más microservicios luego
+    // Epacio para servicios
   ],
 })
 
