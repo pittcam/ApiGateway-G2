5f30bd997438ee898cf6e2a74cdcc51ed438c589
Juanba2002 <bajuan00@gmail.com>
2025-04-10 22:05:06 -0500

    ApiGateway

---
+++ index.js
@@ -0,0 +1,26 @@
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+import { loadSchema } from '@graphql-tools/load'
+import { stitchSchemas } from '@graphql-tools/stitch'
+import { UrlLoader } from '@graphql-tools/url-loader'
+
+const schema = await stitchSchemas({
+  subschemas: [
+    {
+      schema: await loadSchema('http://localhost:5000/graphql', {
+        loaders: [new UrlLoader()],
+      }),
+    }
+    // Aquí puedes agregar más microservicios luego
+  ],
+})
+
+const server = new ApolloServer({
+  schema,
+})
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+})
+
+console.log(`🚀 API Gateway listo en ${url}`)
